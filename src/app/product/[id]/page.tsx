d3937b45f3e544415f7e709e7e5697a002d97d7a
Fatima Salman <fati.salman31@gmail.com>
2025-01-21 17:16:59 +0500

    Fix dynamic route and type for product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,14 +1,18 @@
-
-
+// src/app/product/[id]/page.tsx
 import ProductDetails from "@/app/components/ProductDetails";
 import { client } from "@/sanity/lib/client";
 import { Product } from "@/types/product";
 
-export default async function Page({ params }: { params: { id: string } }) {
+// This is the correct way to handle the dynamic route for a page in Next.js
+interface PageProps {
+  params: { id: string };
+}
+
+export default async function Page({ params }: PageProps) {
   const product: Product | null = await client.fetch(
     `*[_type=="product" && _id==$id][0]{
       _id,
-     title,
+      title,
       price,
       description,
       productImage,
